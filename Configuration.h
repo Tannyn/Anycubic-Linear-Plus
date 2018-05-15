b381c6fcd3954fec2a8f465fe88f99366c47f885
Tannyn <yieldthefox@gmail.com>
2018-05-14 20:46:55 -0500

    Mesurement adjust
    diagonal rod and delta radius

--- Configuration.h
+++ Configuration.h
@@ -498,7 +498,7 @@
   #define DELTA_PRINTABLE_RADIUS 116.0 // mm
 
   // Center-to-center distance of the holes in the diagonal push rods.
-  #define DELTA_DIAGONAL_ROD 265 // mm
+  #define DELTA_DIAGONAL_ROD 271.5 // mm
 
   // height from z=0 to home position
   #define DELTA_HEIGHT 292 // get this value from auto calibrate
@@ -506,7 +506,7 @@
   #define DELTA_ENDSTOP_ADJ { 0.0, 0.0, 0.0 } // get these from auto calibrate
 
   // Horizontal distance bridged by diagonal push rods when effector is centered.
-  #define DELTA_RADIUS 99.4 //mm  Get this value from auto calibrate
+  #define DELTA_RADIUS 134.4 //mm  Get this value from auto calibrate
 
   // Trim adjustments for individual towers
   // tower angle corrections for X and Y tower / rotate XYZ so Z tower angle = 0
